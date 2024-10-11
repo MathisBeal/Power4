4effe848622d621710fe9e491c9f455be63d8bb2
Xenaros <mathis.beal04@gmail.com>
2024-10-11 11:50:48 +0200

    YEET

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ### First thing to do:
 In your shell:
 ```bash
+pip install --upgrade pip
 pip install -r requirements.txt
 ```
--- Test.py
+++ Test.py
@@ -1,7 +1,42 @@
 print("Hello world!")
 
+import tensorflow as tf
 import pygame as pg
+
+print("\n\n##########################\nTensorFlow version:", tf.__version__)
 print("PyGame version:",pg.__version__)
 
-import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
+
+def sep():
+    print("\n\n##########################################\n")
+
+
+sep()
+predictions = model(x_train[:1]).numpy()
+print("predictions")
+print(predictions)
+
+sep()
+print("tf.nn.softmax(predictions).numpy()")
+print(tf.nn.softmax(predictions).numpy())
+
+sep()
+
+
+
+
+
