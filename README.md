547ceb40e3b1f3fd7b66a0da5f3da888aa5f470c
Xenaros <mathis.beal04@gmail.com>
2024-10-11 10:48:34 +0200

    Removed venv, and added a requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Power4
+
+### First thing to do:
+In your shell:
+```bash
+pip install -r requirements.txt
+```
---
+++ Test.py
@@ -0,0 +1,7 @@
+print("Hello world!")
+
+import pygame as pg
+print("PyGame version:",pg.__version__)
+
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
